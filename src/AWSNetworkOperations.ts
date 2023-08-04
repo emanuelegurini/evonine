b083dc2a062a3cbb6d33146a26902adb8723c46a
EmanueleGurini <emanuele.gurini89@gmail.com>
2023-08-04 22:08:08 +0200

    Update: status stack filters

--- src/AWSNetworkOperations.ts
+++ src/AWSNetworkOperations.ts
@@ -2,11 +2,13 @@ import { execSync } from "child_process";
 import { IAWSNetworkOperator } from "./INetworkOperator";
 
 export class AWSNetworkOperator implements IAWSNetworkOperator {
+  private statusFilter: string = `CREATE_IN_PROGRESS CREATE_FAILED CREATE_COMPLETE ROLLBACK_IN_PROGRESS ROLLBACK_FAILED ROLLBACK_COMPLETE DELETE_IN_PROGRESS DELETE_FAILED DELETE_COMPLETE UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_COMPLETE UPDATE_FAILED UPDATE_ROLLBACK_IN_PROGRESS UPDATE_ROLLBACK_FAILED UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE REVIEW_IN_PROGRESS IMPORT_IN_PROGRESS IMPORT_COMPLETE IMPORT_ROLLBACK_IN_PROGRESS IMPORT_ROLLBACK_FAILED IMPORT_ROLLBACK_COMPLETE`;
+
   /**
    *
    */
   public fetchStackList(region: string): string {
-    const command = `aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE ROLLBACK_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE --region ${region} --query 'sort_by(StackSummaries, &StackName)[*].[StackName, StackStatus, DriftInformation.StackDriftStatus, DriftInformation.LastCheckTimestamp]' --output table`;
+    const command = `aws cloudformation list-stacks --stack-status-filter ${this.statusFilter} --region ${region} --query 'sort_by(StackSummaries, &StackName)[*].[StackName, StackStatus, DriftInformation.StackDriftStatus, DriftInformation.LastCheckTimestamp]' --output table`;
     const result = execSync(command, { encoding: "utf-8" });
     return result;
   }
@@ -24,7 +26,7 @@ export class AWSNetworkOperator implements IAWSNetworkOperator {
    *
    */
   public getAllStackWithStatus(region: string): string {
-    const command = `aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE ROLLBACK_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE --region ${region} --query 'sort_by(StackSummaries, &StackName)[*].[StackName, StackStatus, DriftInformation.StackDriftStatus, DriftInformation.LastCheckTimestamp]' --output table`;
+    const command = `aws cloudformation list-stacks --stack-status-filter ${this.statusFilter} --region ${region} --query 'sort_by(StackSummaries, &StackName)[*].[StackName, StackStatus, DriftInformation.StackDriftStatus, DriftInformation.LastCheckTimestamp]' --output table`;
     const result = execSync(command, { encoding: "utf-8" });
     return result;
   }
@@ -33,7 +35,7 @@ export class AWSNetworkOperator implements IAWSNetworkOperator {
    *
    */
   public getAllDriftedStacks(region: string): string {
-    const command = `aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE ROLLBACK_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE --region ${region} --query 'sort_by(StackSummaries, &StackName)[*].[StackName, StackStatus, DriftInformation.StackDriftStatus, DriftInformation.LastCheckTimestamp]' --output table | grep DRIFTED`;
+    const command = `aws cloudformation list-stacks --stack-status-filter ${this.statusFilter} --region ${region} --query 'sort_by(StackSummaries, &StackName)[*].[StackName, StackStatus, DriftInformation.StackDriftStatus, DriftInformation.LastCheckTimestamp]' --output table | grep DRIFTED`;
     const result = execSync(command, { encoding: "utf-8" });
     return result;
   }
